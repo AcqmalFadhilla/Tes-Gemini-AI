2a34794b8dfba4ba291e31574d732f98c37cccc5
Muhammad Acqmal Fadhilla Latief <109632348+AcqmalFadhilla@users.noreply.github.com>
2023-12-17 14:02:19 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import textwrap
+import pathlib
+import google.generativeai as genai
+from IPython.display import display
+
+def model_gemini(name):
+    # setup api model
+    genai.configure(api_key='AIzaSyAWX5_0biZQ8DkS7vYulu1jWXI6E-Sm_qY')
+
+    for m in genai.list_models():
+        if 'generateContent' in m.supported_generation_methods:
+            print(m.name)
+
+    model = genai.GenerativeModel('gemini-pro')
+
+    while True:
+        input_user = input("masukkan prompt:")
+        response = model.generate_content(input_user)
+        print(response.text)
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    model_gemini('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
